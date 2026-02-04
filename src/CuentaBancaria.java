6437b9d9cd2b390b967cab6cd6f7a51db01a9500
yaiza22 <yaizas719@gmail.com>
2026-02-04 09:51:04 -0500

    Se crean metodos getters y setters, y validaciones sobre actualizaciones de saldopara consignar y retirar

--- src/CuentaBancaria.java
+++ src/CuentaBancaria.java
@@ -14,4 +14,22 @@ public class CuentaBancaria {
     public int getSaldo() {
         return saldo;
     }
+
+    public void consignar(int valorAConsignar) {
+        // Invariantes: validan que se cumpla con los aspectos de negocio
+        if(valorAConsignar <= 0) {
+            throw new IllegalArgumentException("El valor a consignar no es válido.");
+        }
+        this.saldo += valorAConsignar;
+    }
+
+    public void retirar(int valorARetirar) {
+        if(valorARetirar <= 0) {
+            throw new IllegalArgumentException("El valor a retirar no es válido.");
+        }
+        if(valorARetirar > this.saldo) {
+            throw new IllegalStateException("El valor a retirar supera su saldo actual.");
+        }
+        this.saldo -= valorARetirar;
+    }
 }
