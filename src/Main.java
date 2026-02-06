282d7d65e981c2781a64c66441110208d7ae83e8
yaiza22 <yaizas719@gmail.com>
2026-02-06 09:38:18 -0500

    Implementacion de notificacion por messeger

--- src/Main.java
+++ src/Main.java
@@ -5,10 +5,12 @@ public class Main {
         cuenta1.consignar(150);
         System.out.println("El saldo de la cuenta es: " + cuenta1.getSaldo());
 
-        Notificacion[] notificaciones = new Notificacion[2];
+        Notificacion[] notificaciones = new Notificacion[4];
         notificaciones[0] = new NotificacionCorreo("yaiza.sanchez@unibague.edu.co");
         notificaciones[1] = new NotificacionSMS("3209623204");
+        notificaciones[3] = new NotificacionMessenger("@CamiloMurcia56");
         notificaciones[0].enviarMensaje("Hola, estimado estudiante.");
         notificaciones[1].enviarMensaje("¿Sin novia?, en Tigo tenemos los mejores planes de amor y amistad, pasate ya!");
+        notificaciones[3].enviarMensaje("Hola camilo, como estas? :)");
     }
 }
